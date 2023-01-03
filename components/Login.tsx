609b918c9e6e20f3af821270b76a4a3468600d26
Paul Maier <pauljustus279@gmail.com>
2023-01-03 17:11:18 +0100

    Improved auth flow

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,93 +1,41 @@
-import React, { useEffect } from "react";
-import { OAuthProvider, signInWithPopup } from "firebase/auth";
-import {
-  auth,
-  db,
-  githubOAuthProvider,
-  microsoftOAuthProvider,
-} from "../firebase";
-import {
-  collection,
-  query,
-  where,
-  getDocs,
-  getCountFromServer,
-  doc,
-  getDoc,
-} from "@firebase/firestore";
+import React from "react";
+import { signInWithPopup } from "firebase/auth";
+import { auth, githubOAuthProvider, microsoftOAuthProvider } from "../firebase";
 import useAuth from "../hooks/useAuth";
-import router from "next/router";
-import { User } from "../interfaces/user";
 
 const Auth = () => {
-  const { isLoggedIn, user, logout } = useAuth();
+  const { isLoggedIn, user } = useAuth();
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      if (isLoggedIn && user) {
-        redirect(user).then((path) => {
-          router.push(path);
-        });
-      }
-    }
-  }, [isLoggedIn, user]);
-
-  async function redirect(user: User): Promise<string> {
-    const userRole = await getUserRole(user.uid);
-
-    if (userRole === "admin") {
-      return "/admin";
-    } else if (userRole === "assistant") {
-      return "/assistant";
-    } else {
-      return "/runner";
-    }
-  }
-
-  async function getUserRole(uid: string): Promise<string> {
-    const userRole = await getDoc(doc(db, "user-roles", uid));
-    const role = userRole.data()?.role || "";
-    return role;
+  if (isLoggedIn && user) {
+    return <></>;
   }
 
   const handleRunnerAuth = async () => {
-    signInWithPopup(auth, microsoftOAuthProvider)
-      .then((result) => {
-        const credential = OAuthProvider.credentialFromResult(result);
-        const accessToken = credential?.accessToken;
-        const idToken = credential?.idToken;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    signInWithPopup(auth, microsoftOAuthProvider).catch((error) => {
+      console.log(error);
+    });
   };
 
   const handleAdminAuth = async () => {
-    signInWithPopup(auth, githubOAuthProvider)
-      .then((result) => {
-        const credential = OAuthProvider.credentialFromResult(result);
-        const accessToken = credential?.accessToken;
-        const idToken = credential?.idToken;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    signInWithPopup(auth, githubOAuthProvider).catch((error) => {
+      console.log(error);
+    });
   };
 
   const handleAssistantAuth = async () => {
-    console.log("Assistant");
+    signInWithPopup(auth, githubOAuthProvider).catch((error) => {
+      console.log(error);
+    });
   };
 
   return (
     <div>
       <h1>Anmelden</h1>
-      {!isLoggedIn && (
-        <div>
-          <button onClick={() => handleRunnerAuth()}>Läufer</button>
-          <button onClick={() => handleAssistantAuth()}>Assistent</button>
-          <button onClick={() => handleAdminAuth()}>Admin</button>
-        </div>
-      )}
+      <div>
+        <button onClick={() => handleRunnerAuth()}>Läufer</button>
+        <button onClick={() => handleAssistantAuth()}>Assistent</button>
+        <button onClick={() => handleAdminAuth()}>Admin</button>
+      </div>
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,49 @@
 import Title from "../components/Title";
 import Login from "../components/Login";
 import Head from "../components/Head";
+import Loading from "../components/Loading";
+import useAuth from "../hooks/useAuth";
+import { User } from "../interfaces/user";
+import { getDoc, doc } from "firebase/firestore";
+import router from "next/router";
+import { useEffect } from "react";
+import { db } from "../firebase";
 
 export default function Home() {
+  const { isLoggedIn, user } = useAuth();
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      if (isLoggedIn && user) {
+        redirect(user).then((path) => {
+          router.push(path);
+        });
+      }
+    }
+  }, [isLoggedIn, user]);
+
+  async function redirect(user: User): Promise<string> {
+    const userRole = await getUserRole(user.email);
+
+    if (userRole === "admin") {
+      return "/admin";
+    } else if (userRole === "assistant") {
+      return "/assistant";
+    } else {
+      return "/runner";
+    }
+  }
+
+  async function getUserRole(email: string): Promise<string> {
+    const userRole = await getDoc(doc(db, "user-roles", email));
+    const role = userRole.data()?.role || "";
+    return role;
+  }
+
+  if (isLoggedIn && user) {
+    return <Loading />;
+  }
+
   return (
     <>
       <Head title="Anmeldung" />
