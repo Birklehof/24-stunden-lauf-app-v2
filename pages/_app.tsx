14b231507156a019781b03aafa9d1cafd7f954a4
Paul Maier <pauljustus279@gmail.com>
2023-01-03 17:55:42 +0100

    Montserrat font and daisyui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Montserrat } from "@next/font/google";
+
+const montserrat = Montserrat();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --montserrat-font: ${montserrat.style.fontFamily};
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#004e47",
+          secondary: "#e4f1de",
+          accent: "#d9e5ec",
+          neutral: "#ffffff",
+          "base-100": "#FFFFFF",
+          success: "#B7C82C",
+        },
+      },
+    ],
   },
-  plugins: [],
+  theme: {
+    fontFamily: {
+      sans: ["var(--montserrat-font)", ...fontFamily.sans],
+    },
+  },
+  plugins: [require("daisyui")],
 };
