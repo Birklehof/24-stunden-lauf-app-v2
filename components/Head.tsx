b8dd754d92765352fa6935403938036a1d3cddd6
Paul Maier <pauljustus279@gmail.com>
2023-02-14 20:40:44 +0100

    meta tags and favicon

--- components/Head.tsx
+++ components/Head.tsx
@@ -11,6 +11,14 @@ export default function Header({ title }: HeadProps) {
     <>
       <Head>
         <title>{pageTitle}</title>
+        <meta
+          name="description"
+          content="The official Birklehof 24 Stunden Lauf Portal."
+        />
+        <meta name="keywords" content="Birklehof, 24-Stunden-Lauf" />
+        <meta name="author" content="Schule Birklehof e.V." />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/favicon.webp" />
       </Head>
     </>
   );
