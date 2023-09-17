41f9c5eb201c4d3c88d61216f5b6f2cf3bd17b26
Paul Maier <pauljustus279@gmail.com>
2023-09-17 22:29:48 +0200

    Removed workarounds

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  webpack: (webpackConfig, { webpack }) => {
-    webpackConfig.resolve.extensionAlias = {
-      '.js': ['.ts', '.tsx', '.js', '.jsx'],
-      '.ts': ['.js', '.jsx', '.ts', '.tsx'],
-      '.tsx': ['.js', '.jsx', '.ts', '.tsx'],
-    };
-    return webpackConfig;
-  },
 };
 
 module.exports = nextConfig;
