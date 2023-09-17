3630f9d2d057c40f57e6d7c4dced740d3bdd4c6e
Paul Maier <pauljustus279@gmail.com>
2023-09-17 22:10:32 +0200

    Possible fix for deployment issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (
+    webpackConfig,
+    { webpack },
+  ) => {
+    webpackConfig.resolve.extensionAlias = {
+      '.js': ['.ts', '.tsx', '.js', '.jsx'],
+      '.ts': ['.js', '.jsx', '.ts', '.tsx'],
+      '.tsx': ['.js', '.jsx', '.ts', '.tsx'],
+    };
+    return webpackConfig;
+  },
 }
 
 module.exports = nextConfig
