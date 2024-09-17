90f48e5bfb147c7325a13fcd3ea327cb0c3ab4a1
Paul Maier <github@paulmaier.online>
2024-09-17 17:50:15 +0200

    Hide SearchBarPlaceholder for Apple

--- components/SearchBarPlaceholder.tsx
+++ components/SearchBarPlaceholder.tsx
@@ -1,13 +1,13 @@
 export default function SearchBarPlaceholder() {
   return (
     <div
-      className="search-bar !relative !left-0 !z-0 !translate-x-0"
+      className="search-bar !relative !left-0 !z-0 !translate-x-0 opacity-0"
       aria-hidden
     >
       <input
         disabled
         aria-hidden
-        className="input-bordered input rounded-box w-10 grow bg-accent opacity-0"
+        className="input-bordered input rounded-box w-10 grow bg-accent"
         placeholder="Suchen ..."
       />
     </div>
