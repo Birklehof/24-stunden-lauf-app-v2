510e934d8c578c0e30b4b5eb3c9b0af1feee1bd2
Paul Maier <contact@paulmaier.online>
2025-09-20 02:03:25 +0200

    Remove cypress

--- cypress/e2e/ui/login.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Login', () => {
-  it('Shows Login', function () {
-    cy.visit('http://localhost:3000/');
-
-    // UI should have two login buttons
-    cy.contains('Läufer').should('be.visible');
-    cy.contains('Helfer').should('be.visible');
-  });
-});
