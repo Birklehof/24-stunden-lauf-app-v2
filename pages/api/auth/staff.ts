43c3173510f6bf83485205bd1568e7901cd3e012
Paul Maier <github@paulmaier.online>
2026-09-24 11:47:38 +0200

    test husky setup

--- pages/api/auth/staff.ts
+++ pages/api/auth/staff.ts
@@ -35,6 +35,7 @@ export default async function handler(
       .createCustomToken(
         process.env.NEXT_PUBLIC_ASSISTANT_ACCOUNT_UID || 'some-uid',
         {
+          // @ts-ignore
           role: 'assistant',
         }
       )
